d63d385d41ee30b04ca656cbd24c8ce84adb3626
greg goldman <gregory.goldman97@gmail.com>
2021-06-23 10:34:48 -0400

    Build the product model
    This creates required fields for the product schema

--- app/models/product.js
+++ app/models/product.js
@@ -1,5 +1,18 @@
 const mongoose = require('mongoose')
 
 const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    reuired: true
+  }
 })
 module.exports = mongoose.model('Product', productSchema)
