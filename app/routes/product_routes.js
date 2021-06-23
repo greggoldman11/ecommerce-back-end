8bc4fda467bf26af1d9ea4212c5ddd4c97f9f89f
OneWay-Laye <tayloralaye@gmail.com>
2021-06-23 10:07:28 -0400

    This commit will create product routes page and adds it to the server

---
+++ app/routes/product_routes.js
@@ -0,0 +1,27 @@
+// Require express
+const express = require('express')
+
+// We will require our product model
+const Product = require('./../models/product')
+
+// require custom errors
+const customErrors = require('../../lib/custom_errors')
+
+const router = express.Router()
+
+// Create product
+router.post()
+
+// Index all products
+router.get()
+
+// Show product
+router.get()
+
+// Update product
+router.patch()
+
+// This will delete book
+router.delete()
+
+module.exports = router
